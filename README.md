248a1c8648cea6e18985676c53cab90103108f80
unknown <adhovi@gmail.com>
2021-04-02 18:01:28 +0600

    uploading server

---
+++ README.md
@@ -0,0 +1,17 @@
+# Bazar-Sodai-Server
+
+This is the server side code of Bazar-Sodai. Bazar-Sodai is a online store, where you can buy product online and also track your orders. This project also has admin panel so that new product can be added, deleted.
+
+For this project I have used Express JS and Mongo DB in server side. Client side is made with react JS.
+
+### `Live Link`
+
+Firebase Hosting: [Bazar-Sodai Firebase](https://bazar-sodai.web.app)
+
+### `Contact Me`
+
+Connect Me on [Facebook](https://facebook.com/adhovi)
+
+### `Client-Side-Repository`
+
+[Bazar-Sodai-Client](https://github.com/Porgramming-Hero-web-course/full-stack-client-adhovi)
---
+++ index.js
@@ -0,0 +1,84 @@
+const express = require("express");
+
+const cors = require("cors");
+const MongoClient = require("mongodb").MongoClient;
+const ObjectId = require("mongodb").ObjectID;
+const { ObjectID } = require("bson");
+
+require("dotenv").config();
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", function (req, res) {
+  res.send("hello world");
+});
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.6qte7.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+client.connect((err) => {
+  const productCollection = client.db("bazar-sodai").collection("products");
+
+  app.post("/addProduct", (req, res) => {
+    const newProduct = req.body;
+    productCollection.insertOne(newProduct).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+  app.get("/products", (req, res) => {
+    productCollection.find().toArray((err, items) => {
+      res.send(items);
+    });
+  });
+  app.get("/product/:id", (req, res) => {
+    productCollection
+      .find({ _id: ObjectID(req.params.id) })
+      .toArray((err, items) => {
+        res.send(items);
+      });
+  });
+
+  app.delete("/deleteProduct/:id", (req, res) => {
+    productCollection
+      .deleteOne({ _id: ObjectId(req.params.id) })
+      .then((result) => {
+        res.send(result.deletedCount > 0);
+      });
+  });
+
+  const orderCollection = client.db("bazar-sodai").collection("orders");
+
+  app.post("/addOrder", (req, res) => {
+    const newOrder = req.body;
+    orderCollection.insertOne(newOrder).then((result) => {
+      res.send(result.insertedCount > 0);
+    });
+  });
+
+  app.get("/orders", (req, res) => {
+    orderCollection.find({ email: req.query.email }).toArray((err, items) => {
+      res.send(items);
+    });
+  });
+
+  app.delete("/deleteOrder/:id", (req, res) => {
+    orderCollection
+      .deleteOne({ _id: ObjectId(req.params.id) })
+      .then((result) => {
+        res.send(result.deletedCount > 0);
+      });
+  });
+
+  // perform actions on the collection object
+  //   client.close();
+});
+
+app.listen(5055, (err) => {
+  console.log("Listening");
+});
